7f31ff2a9d961b9676f3e45a0ec9aaa44902b622
chahdi <alaeddine.chahdi@edu.esiee.fr>
2023-02-01 14:56:49 +0100

    change adress ip"

--- testPing/testPing.sh
+++ testPing/testPing.sh
@@ -1,6 +1,6 @@
 for i in {1..255}
 do
-  ping -W 1 -c 1 10.93.161.45.$i 2> /dev/null
+  ping -W 1 -c 1 10.93.161.$i 2> /dev/null
   if [ $? -eq 0 ]; then
     echo "10.93.161.45.$i est en ligne" >> online.txt
   else
